842ab285e022f827cee2002c72a617ade393544f
joao-asp <joao.paranhos@students.ic.unicamp.br>
2026-07-25 09:41:34 -0300

    fix: atualiza configuração da instância EC2 para usar AMI específica e corrige nome da tag

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -1,11 +1,12 @@
 resource "aws_instance" "sanko_server" {
-  ami           = "ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20260610" 
-  instance_type = "t3.micro" # Altere se sua instância usar outro tipo (ex: t2.micro, t3.small)
+  ami           = "ami-0d7405d05f836d0d4" 
+  instance_type = "t3.micro"            
   
-  key_name = aws_key_pair.sanko_ssh_key.key_name
+  # Se a sua máquina manual não usa essa key_name do Terraform, remova ou comente esta linha por enquanto:
+  # key_name = aws_key_pair.sanko_ssh_key.key_name
 
   tags = {
-    Name = "sanko-app-server"
+    Name = "Sanko-App"
   }
 }
 
